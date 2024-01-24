c1a1c0a887e89d18a4f1a87ba75a311c829e0ba0
pyscripter99 <retzerrt@gmail.com>
2024-01-24 17:49:35 +1000

    Structure LoadPlugin function

--- internal/lua/lua.go
+++ internal/lua/lua.go
@@ -3,3 +3,8 @@ package lua
 import lua "github.com/yuin/gopher-lua"
 
 var l *lua.LState
+
+// Loads a plugin by name from micropad plugin directory
+func LoadPlugin(name string) PluginMeta {
+	return PluginMeta{}
+}
