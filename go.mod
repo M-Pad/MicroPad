ee8958b01230e97a2c6d293f2a8888229a7b9da8
pyscripter99 <retzerrt@gmail.com>
2024-01-24 19:02:54 +1000

    Check if plugin exists in LoadPlugin

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/M-Pad/MicroPad
 go 1.21.5
 
 require github.com/yuin/gopher-lua v1.1.1
+
+require github.com/kirsle/configdir v0.0.0-20170128060238-e45d2f54772f // indirect
--- internal/lua/lua.go
+++ internal/lua/lua.go
@@ -1,10 +1,39 @@
 package lua
 
-import lua "github.com/yuin/gopher-lua"
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"github.com/kirsle/configdir"
+	lua "github.com/yuin/gopher-lua"
+)
 
 var l *lua.LState
 
 // Loads a plugin by name from micropad plugin directory
-func LoadPlugin(name string) PluginMeta {
-	return PluginMeta{}
+func LoadPlugin(name string) (*PluginMeta, error) {
+	paths := []string{
+		filepath.Join(name, "plugin.lua"),                                               // Current directory; usefull for plugin dev
+		filepath.Join(configdir.LocalConfig("micropad"), "plugins", name, "plugin.lua"), // Usual plugin location
+	}
+
+	var pluginPath string
+	for _, path := range paths {
+		_, err := os.Stat(path)
+		if !errors.Is(err, os.ErrNotExist) {
+			return nil, err
+		}
+
+		if err != nil {
+			pluginPath = path
+			break
+		}
+	}
+
+	if pluginPath == "" {
+		return nil, os.ErrNotExist
+	}
+
+	return nil, nil
 }
